badd949f6b4273363535265af3bea3e2dfe487c0
AdamGriffiths31 <adam.j.griffiths@outlook.com>
2023-10-14 16:17:22 +0100

    141

---
+++ Solutions/_141.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head) {
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main(){
+    cout << "Start\n";
+    ListNode *head = new ListNode(3);
+    ListNode *node1 = new ListNode(2);
+    ListNode *node2 = new ListNode(0);
+    ListNode *node3 = new ListNode(-4);
+    head->next = node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node1;
+    bool result = hasCycle(head);
+    cout << "result = " << result << "\n";
+}
